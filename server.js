8472ddc3df01f56fcb0e8cbff4e03953be9294ba
irlanvila <irlvnavila@gmail.com>
2022-04-15 13:32:49 +0700

    connect to mdb cloud

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+dotenv.config();
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MDB_USERNAME}:${process.env.MDB_PASSWORD}@cluster0.kgy6g.mongodb.net/Cluster0?retryWrites=true&w=majority`
+  )
+  .then(() => console.log("MongoDB Successfully Connected!"))
+  .catch((err) => console.log(err));
 
 app.listen(PORT);
